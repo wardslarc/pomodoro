94a943ad627ef49c0b1dd3c4b76f90625819f5d5
wardslarc <cralsdale@gmail.com>
2025-11-01 10:08:49 +0800

    update 7.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { tempo } from "tempo-devtools/dist/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
-  plugins: [
-    react(),
-    tempo(),
-  ],
+  plugins: [react(), tempo()],
   resolve: {
     preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // 👈 this is the key part
+  },
   server: {
-    // @ts-ignore
     allowedHosts: true,
-  }
+  },
 });
